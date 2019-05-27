003fa9a6eebd39175e49b22e056fcdd7f0ff25b1
Kapil Chavhan <kapilchavhan@Kapils-MacBook-Pro.local>
2019-05-27 09:06:06 -0700

    commiting code

--- Main.java
+++ Main.java
@@ -4,10 +4,19 @@ public class Main {
 
         // have any name, don't use predefined methods for now. input "Kapil ChavhAN". output printing as capital letters. convert capitala letter as small letters
         // and display in console
+        String name = "KapiL alskSDFGHdfdkn 8927345][p";
+        StringBuilder UpperCase = new StringBuilder("");
+        for(int i = 0; i< name.length(); i++){
+            if(97 <= (int) name.charAt(i) && 122 >= (int) name.charAt(i)){
+                char inst = (char) ((int) name.charAt(i) - 32);
+                UpperCase.append(inst);
+            }else {
+                UpperCase.append(name.charAt(i));
+            }
+        }
+        System.out.println(UpperCase);
 
 
-        System.out.println("First Application");
-        System.out.println("Hello World!");
     }
 }
 
