f9d30b9161cf789e8053a69a3e025762f8dfa754
Kapil Chavhan <kapilchavhan@Kapils-MacBook-Pro.local>
2019-05-27 07:36:16 -0700

    commiting the message

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // have any name, don't use predefined methods for now. input "Kapil ChavhAN". output printing as capital letters. convert capitala letter as small letters
+        // and display in console
+
+
         System.out.println("First Application");
         System.out.println("Hello World!");
     }
