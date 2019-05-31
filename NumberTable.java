4d42463211c2d3c99e2597f3b80498fb11d0b1fd
Kapil Chavhan <kapilchavhan@Kapils-MacBook-Pro.local>
2019-05-30 19:46:45 -0700

    Adding Third Table in Git

---
+++ NumberTable.java
@@ -0,0 +1,15 @@
+class NumberTable
+{
+    public static void main(String[] args) {
+        System.out.println("Hi");
+
+
+        System.out.println(" Third Table");
+        for ( int i =0 ; i<=10; i++)
+        {
+            int a = 3*i;
+            System.out.println(a);
+
+        }
+    }
+}
